40602bf1107f231c7dd458c1c1602932c538bfc8
LizzieK549 <klizzy630@gmail.com>
2024-10-25 22:14:51 -0500

    Week8 project
    Elizabeth Kerr, Week 8, 10/25

--- program_2.py
+++ program_2.py
@@ -4,6 +4,8 @@
 # Convert the sentence to a string in which the words are separated by spaces, 
 # and the first word starts with an uppercase.  
 # For example the string "StopAndSmellTheRoses" would be converted to "Stop and smell the roses."
+from html.parser import charref
+
 
 # Start your changes on line 13
 
@@ -11,8 +13,22 @@ def word_separator(sentence):
 
     new_sentence = ""
     #    Add your logic here
+    Result = ''
 
-    return new_sentence.strip()
+    User_input = input('Enter a sentence with no spaces but uppercase the first letter of every word: ')
+
+    Result = Result + User_input[0]
+
+    for i in range (1, len(User_input)):
+        char = User_input[i]
+        if char.isupper():
+            Result += ' ' + char.lower()
+        else:
+            Result += char.lower()
+
+    return Result
+
+
 
 # Example usage
 
